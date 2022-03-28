b4afd337f4d4f1451f428abc58c6ec3153ef7318
annkathrinfi <99340383+annkathrinfi@users.noreply.github.com>
2022-03-28 09:14:25 +0200

    Update run.py

--- workspace/app/run.py
+++ workspace/app/run.py
@@ -38,7 +38,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponseData', engine)
 
 # load model
-model = joblib.load('/models/classifier.pkl')
+model = joblib.load('../models/classifier.pkl')
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
